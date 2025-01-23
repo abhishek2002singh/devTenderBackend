8710d1fb4cc7fecb4f722696a20f0947ff3a13f0
abhishek singh <singhsabhishek23@gmail.com>
2025-01-23 09:33:24 +0530

    Your commit message

---
+++ src/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const app = express();
+const dbConnect = require('./config/database');
+const cookieParser = require('cookie-parser')
+
+const dotenv = require('dotenv');
+
+const port = process.env.PORT || 3000
+const path = require('path')
+
+const _dirname = path.resolve();
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:5173', 
+    credentials:true,            //access-control-allow-credentials:true
+  
+}
+app.use(cors(corsOptions));
+app.use(express.json())
+app.use(cookieParser())
+
+
+//routes
+const authRouth = require('./routes/auth')
+const profileRouter = require('./routes/profile')
+const requestRouter = require('./routes/request');
+const userRouter = require('./routes/user');
+
+
+app.use('/' ,authRouth)
+app.use('/' ,profileRouter)
+app.use('/' ,requestRouter)
+app.use('/',userRouter)
+
+
+
+
+
+dbConnect().then(() => {
+    console.log('Connection successful');
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+      });
+}).catch(err => {
+    console.error('Database connection failed:', err);
+});
+
+
+//yadavajjet05(user)  cHuG5KUCLr0ynu2G(password)
+
+
+ 
---
+++ src/config/database.js
@@ -0,0 +1,44 @@
+const mongoose  = require('mongoose')
+
+// Importing the environment variables using the dotenv library
+// require("dotenv").config();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+//use can use both 
+
+// Defining a function to connect to the database
+// const dbConnect = async() => {
+// 	// Connecting to the database using the provided URL from the environment variables
+// 	mongoose.connect(process.env.DATABASE_URL, {
+// 			useNewUrlParser: true,
+// 			useUnifiedTopology: true,
+// 		})
+// 		// If the connection is successful, log a success message
+// 		.then(() => console.log("DB CONNECTION SUCCESS"))
+// 		// If there are issues connecting to the database, log an error message and exit the process
+// 		.catch((err) => {
+// 			console.log(`DB CONNECTION ISSUES`);
+// 			console.error(err.message);
+// 			process.exit(1);
+// 		});
+// };
+
+
+const dbConnect = async() => {
+	// Connecting to the database using the provided URL from the environment variables
+	try{ 
+		await mongoose.connect(process.env.DATABASE_URL)
+		console.log('Database connected successfully');
+
+	}catch(error){
+		console.error('Database connection error:', error);
+        throw error;
+	}
+	
+};
+
+// Exporting the dbConnect function for use in other files
+module.exports = dbConnect;
+
