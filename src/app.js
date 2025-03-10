00bf845e2cd1103fe084df8f36c94c900259b12b
abhishek singh <singhsabhishek23@gmail.com>
2025-03-10 12:47:14 +0530

    ad d origin

--- src/app.js
+++ src/app.js
@@ -81,7 +81,7 @@ const fileUpload = require("express-fileupload");
 
 // Middlewares
 app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://devtenderfrontend.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
